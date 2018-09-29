781cdefa22ffbf987611356aedacdaf910f8befc
Praffulsingh9 <prafful.singh017@gmail.com>
2018-09-30 01:12:35 +0530

    App component updated with GoalListcomponent

--- goalshandler/src/components/App.js
+++ goalshandler/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../App.css';
 import AddGoal from './AddGoal';
+import GoalList from './GoalList';
 import {firebaseApp} from '../firebase';
 import {connect} from 'react-redux';
 class App extends Component {
@@ -14,7 +15,7 @@ class App extends Component {
       <div className="App">
         <h3>Goals</h3>
         <AddGoal/>
-        <div>Goals List</div>
+        <GoalList />
         <button
         className="btb btn-danger"
         onClick={()=>this.signOut()}>Sign Out</button>
