d84362b9d1d37e1da457925d45657040b9cf1cbe
Praffulsingh9 <prafful.singh017@gmail.com>
2018-09-30 01:13:20 +0530

    A component to store goallist added

---
+++ goalshandler/src/components/GoalList.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import {goalRef} from '../firebase';
+class GoalList extends Component {
+  
+    componentDidMount(){
+        goalRef.on('value',snap => {
+            let goals = [];
+            snap.forEach(goal=>{
+                const {email,title} = goal.val();
+                
+   
+                goals.push({email,title});
+            })
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default GoalList;
