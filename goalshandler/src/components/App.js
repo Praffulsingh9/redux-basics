e5c7782f7e31612dcd42f08ae5e75a9f2477dae2
Praffulsingh9 <prafful.singh017@gmail.com>
2018-09-29 21:02:00 +0530

    redirection added

--- goalshandler/src/components/App.js
+++ goalshandler/src/components/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import '../App.css';
+import {firebaseApp} from '../firebase';
 
 class App extends Component {
+
+  signOut(){
+      firebaseApp.auth().signOut();
+  }
+
   render() {
     return (
       <div className="App">
-        
+        App
+        <button
+        className="btb btn-danger"
+        onClick={()=>this.signOut()}>Sign Out</button>
       </div>
     );
   }
